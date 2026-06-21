a9653b6e212d8bf0ed188ef7a21a426157576827
Zakaria Abdallah Halim <88517236+Zecoabdo16@users.noreply.github.com>
2026-06-21 09:52:25 +0300

    Revert "create the structure"

--- 26-06-14-Hospital-patient-assistant/technical_structures/hisham_project/RAFIQ/adapters/emr_adapter.py
+++
@@ -1,9 +0,0 @@
-class EMRAdapter:
-    def get_patient_history(self, patient_id: str) -> dict:
-        # Later, queries Postgres. Now, hardcode the "CKD Patient" story.
-        return {
-            "name": "أحمد محمد",
-            "chronic_conditions": ["CKD", "Hypertension"],
-            "allergies": ["Penicillin"],
-            "medications": ["Warfarin"]
-        }
--- 26-06-14-Hospital-patient-assistant/technical_structures/hisham_project/RAFIQ/adapters/his_adapter.py
+++
@@ -1,7 +0,0 @@
-class HISAdapter:
-    def check_availability(self, doctor_id: str, date: str) -> dict:
-        # In Week 3, this will query PostgreSQL. For now, return mock.
-        return {"available_slots": ["10:00 AM", "02:30 PM"], "doctor_name": "Dr. Tarek"}
-
-    def book_appointment(self, patient_id: str, slot: str) -> dict:
-        return {"status": "success", "booking_id": "APT-9921"}
